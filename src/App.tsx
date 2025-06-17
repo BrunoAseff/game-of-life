c5f896aeafafa313321bd8d6b5f4c05503447b13
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-06-17 12:49:41 +0000

    feat: add square'

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
+  const canvasRef = useRef(null)
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const ctx = canvas.getContext('2d')
+    ctx.strokeStyle = '#fff'
+    ctx.strokeRect(10, 10, 1000, 500)
+  }, [])
 
   return (
+    <main>
+      <div>The game of life</div>
       <div>
-       The game of life
+        <canvas ref={canvasRef} width="1100" height="600" />
       </div>
-     
+    </main>
   )
 }
 
