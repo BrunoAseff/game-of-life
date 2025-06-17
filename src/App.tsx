1d36b13eaa06f702ccad986e6024981fc8f63b70
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-06-17 13:23:32 +0000

    feat: fix square

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,18 @@ function App() {
   useEffect(() => {
     const canvas = canvasRef.current
     const ctx = canvas.getContext('2d')
+    
+    // define a shadow
+    ctx.shadowColor = '#646cff'
+    ctx.shadowBlur = 3
+    ctx.shadowOffsetX = 0
+    ctx.shadowOffsetY = 0
+    
+    // stroke
     ctx.strokeStyle = '#fff'
+    ctx.lineWidth = 2
+    
+    // retângulo
     ctx.strokeRect(10, 10, 1000, 500)
   }, [])
 
