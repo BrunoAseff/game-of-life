6b641880cad94f709f413f816e57f312d7c279f5
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-07-05 13:58:23 +0000

    feat: update game of life

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,44 @@ function App() {
   useEffect(() => {
     const canvas = canvasRef.current
     const ctx = canvas.getContext('2d')
-    
-    // define a shadow
+
+    // shadow
     ctx.shadowColor = '#646cff'
-    ctx.shadowBlur = 3
+    ctx.shadowBlur = 1
     ctx.shadowOffsetX = 0
     ctx.shadowOffsetY = 0
     
     // stroke
     ctx.strokeStyle = '#fff'
-    ctx.lineWidth = 2
-    
-    // retângulo
-    ctx.strokeRect(10, 10, 1000, 500)
+    ctx.lineWidth = 1
+
+
+    const cellSize = 16; 
+    const rows = Math.floor(canvas.height / cellSize);
+    const cols = Math.floor(canvas.width / cellSize);
+
+    const grid = [];
+
+    for (let row = 0; row < rows; row++) {
+      for (let col = 0; col < cols; col++) {
+        const x = col * cellSize;
+        const y = row * cellSize;
+        
+        ctx.strokeRect(x, y, cellSize, cellSize);
+        
+        grid.push({ row, col, x, y, width: cellSize, height: cellSize });
+      }
+    }
+
+    console.log(grid)
+
   }, [])
 
   return (
     <main className='main'>
       <div>The game of life</div>
       <div>
-        <canvas ref={canvasRef} width="1100" height="600" />
+        <canvas ref={canvasRef} width="1200" height="600" />
       </div>
       <StarsBackground/>
     </main>
